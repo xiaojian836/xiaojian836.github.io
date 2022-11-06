ee2d679f6ccdee454bc4c296b3af0d67ae6e3b25
xiaojian <836543356@QQ.COM>
2022-11-06 13:40:17 +0800

    Site updated: 2022-11-06 13:40:17

--- js/title.js
+++ js/title.js
@@ -1,17 +1,1 @@
- /**
-   * 浏览器离开和点击标题样式
-   */
-var OriginTitle = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-    if (document.hidden) {
-        document.title = 'FBL Warning';
-        clearTimeout(titleTime);
-    }
-    else {
-        document.title = OriginTitle;
-        titleTime = setTimeout(function () {
-            document.title = OriginTitle;
-        }, 2000);
-    }
-});
+var titleTime,OriginTitle=document.title;document.addEventListener("visibilitychange",(function(){document.hidden?(document.title="FBL Warning",clearTimeout(titleTime)):(document.title=OriginTitle,titleTime=setTimeout((function(){document.title=OriginTitle}),2e3))}));
